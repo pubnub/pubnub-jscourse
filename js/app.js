465418ef5118f496cc08eedd8d38a4d90b5ae2cd
Frederick Brock <frederick.brock@gmail.com>
2015-03-02 13:41:05 -0800

    new ui

--- js/app.js
+++ js/app.js
@@ -1,48 +1,28 @@
-(function() {
 
-    var output = PUBNUB.$('output'), 
-        input = PUBNUB.$('input'), 
-        button = PUBNUB.$('button'),
-        avatar = PUBNUB.$('avatar'),
-        presence = PUBNUB.$('presence');
 
-    var channel = 'mchat';
-    
-    // Assign a random avatar in random color
-    avatar.className = 'face-' + ((Math.random() * 13 + 1) >>> 0) + ' color-' + ((Math.random() * 10 + 1) >>> 0);
+var Util = {
 
-    var p = PUBNUB.init({
-        subscribe_key: 'sub-c-f762fb78-2724-11e4-a4df-02ee2ddab7fe',
-        publish_key:   'pub-c-156a6d5f-22bd-4a13-848d-b5b4d4b36695'
-    });
+    'updateUserList' : function(pmsg){
 
-    p.subscribe({
-        channel  : channel,
-        callback : function(m) { 
-            output.innerHTML = '<p><i class="' + m.avatar + '"></i><span>' +  m.text.replace( /[<>]/ig, '' ) + '</span></p>' + output.innerHTML; 
-        },
-        presence: function(m){
-            if(m.occupancy > 1) {
-                presence.textContent = m.occupancy + ' people online';
-            } else {
-                presence.textContent = 'Nobody else is online';
+        var userlist = document.querySelector("#userlist");
+        if((pmsg.action === 'leave') || (pmsg.action === 'timeout')){
+            userlist.children.forEach(function(e){
+                if(e.attrbutes['uuid'] === uuid){
+                    e.className = "offline";
+                }
+            })
+        }else if((pmsg.action === 'join') && (pmsg.data)){
+
+            var li = document.createElement('li');
+            var userDiv = document.createElement("div");
+            userDiv.className = 'user-presence-container';
+            if(pmsg.data){ //
+                userDiv.innerHTML = '<p>' + pmsg.data.username + '</p>';
             }
+
+            li.appendChild(userDiv);
+            li.setAttribute("class","online");
+            userlist.appendChild(li);
         }
-    });
-
-    p.bind('keyup', input, function(e) {
-        (e.keyCode || e.charCode) === 13 && publish()
-    });
-
-    p.bind('click', button, publish);
-
-    function publish() {
-        p.publish({
-            channel : channel, 
-            message : {avatar: avatar.className, text: input.value}, 
-            x : (input.value='')
-        });
     }
-
-
-})();
+}
